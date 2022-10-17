5f2d9ec82539fd2d392eba84e75610b684489c02
Babas788 <agi.bastien@hotmail.fr>
2022-10-17 13:41:57 +0200

    page 1

---
+++ front/js/script.js
@@ -0,0 +1,37 @@
+//récupération de l'API
+kanapApi();
+
+creationProduit();
+async function kanapApi() {
+  let produits = await fetch("http://localhost:3000/api/products");
+  return produits.json();
+  console.log(produits);
+}
+
+//injection de mes cartes avec reprise d'API
+
+async function creationProduit() {
+  const style = await kanapApi().then((mesProduits) => {
+    for (let i = 0; i < mesProduits.length; i += 1) {
+      const lien = document.createElement("a");
+      document.getElementById("items").appendChild(lien);
+      lien.href = "product.html?id=${mesProduits[i]._id}";
+
+      const mesArticles = document.createElement("article");
+      lien.appendChild(mesArticles);
+
+      const mesImages = document.createElement("img");
+      mesArticles.appendChild(mesImages);
+      mesImages.src = mesProduits[i].imageUrl;
+      mesImages.alt = mesProduits[i].altTxt;
+
+      const mesTitres = document.createElement("h3");
+      mesArticles.appendChild(mesTitres);
+      mesTitres.innerHTML = mesProduits[i].name;
+
+      const mesDescriptions = document.createElement("p");
+      mesArticles.appendChild(mesDescriptions);
+      mesDescriptions.innerHTML = mesProduits[i].description;
+    }
+  });
+}
