567da8213966f8fa2519b19a324dbd81097b05b8
Babas788 <agi.bastien@hotmail.fr>
2022-10-19 00:14:04 +0200

    detail

--- front/js/product.js
+++ front/js/product.js
@@ -12,17 +12,23 @@ async function kanapApi() {
 
 async function monProduit() {
   const insertion = await kanapApi().then((detail) => {
-    const texte = detail.altTxt;
     const image = detail.imageUrl;
-    const description = detail.description;
-    const prix = detail.price;
     const couleur = detail.colors;
     const monId = detail._id;
 
     const packShot = document.createElement("img");
     document.querySelector(".item__img").appendChild(packShot);
-    packShot.src = image;
-    packShot.alt = texte;
+    packShot.src = detail.imageUrl;
+    packShot.alt = detail.altTxt;
+
+    const title = document.getElementById("title");
+    title.innerHTML = detail.name;
+
+    const prix = document.getElementById("price");
+    prix.innerHTML = detail.price;
+
+    const description = document.getElementById("description");
+    description.innerHTML = detail.description;
   });
 }
 
