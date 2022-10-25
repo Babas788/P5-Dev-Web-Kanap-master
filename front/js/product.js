93a37823fe797eee8ee7910f71e0032504d7d51b
Babas788 <agi.bastien@hotmail.fr>
2022-10-25 15:05:15 +0200

    commit

--- front/js/product.js
+++ front/js/product.js
@@ -4,39 +4,69 @@
 const page = window.location.search;
 const url = new URLSearchParams(page);
 const id = url.get("id");
-
 const myUrl = `http://localhost:3000/api/products/${id}`;
+
+// récupération de mon prix
+let myPrice = 0;
 
 fetch(myUrl)
   .then((response) => response.json())
   .then((data) => create(data));
 
 function create(product) {
-  const image = document.createElement("img");
-  const couleur = product.colors;
-
+  myPicture(product);
   insert(product);
-  child();
-  color();
+  const color = product.colors;
 
-  function child() {
+  //récup prix
+  myPrice = product.price;
+
+  function myPicture(product) {
+    const image = document.createElement("img");
     document.querySelector(".item__img").appendChild(image);
+    image.src = product.imageUrl;
   }
 
   function insert(product) {
-    image.src = product.imageUrl;
     document.getElementById("title").innerHTML = product.name;
     document.getElementById("price").innerHTML = product.price;
     document.getElementById("description").innerHTML = product.description;
   }
 
-  function color() {
-    couleur.forEach((color) => {
-      const option = document.createElement("option");
-      console.log(option);
-      document.getElementById("colors").appendChild(option);
-      option.value = color;
-      option.textContent = color;
-    });
-  }
+  color.forEach((color) => {
+    const option = document.createElement("option");
+    document.getElementById("colors").appendChild(option);
+    option.value = color;
+    option.textContent = color;
+  });
 }
+
+// récupération données bouton panier
+
+const addBasket = document.getElementById("addToCart");
+
+// creation d'un message d'alerte
+
+addBasket.addEventListener("click", (e) => {
+  const productColor = document.getElementById("colors").value;
+  const productQuantity = document.getElementById("quantity").value;
+  if (
+    productColor == null ||
+    productColor == "" ||
+    productQuantity == null ||
+    productQuantity == 0
+  ) {
+    alert("veuillez selectionner une couleur et une quantité valide!");
+  }
+
+  //récupération des données
+
+  let myData = {
+    id: id,
+    color: productColor,
+    price: myPrice,
+    quantity: productQuantity,
+  };
+
+  localStorage.setItem(productColor, JSON.stringify(myData));
+});
--- front/js/script.js
+++ front/js/script.js
@@ -15,6 +15,7 @@ function create(product) {
     const title = document.createElement("h3");
 
     const description = document.createElement("p");
+
     myLink(product);
     myArticle(product);
     picture(product);
@@ -25,21 +26,19 @@ function create(product) {
       document.getElementById("items").appendChild(link);
       link.href = `./product.html?id=${product._id}`;
     }
-    function myArticle(product) {
+
+    function myArticle() {
       link.appendChild(article);
     }
-
     function picture(product) {
       article.appendChild(image);
       image.src = product.imageUrl;
       image.alt = product.altTxt;
     }
-
     function myTitle(product) {
       article.appendChild(title);
       title.innerHTML = product.name;
     }
-
     function myDescription(product) {
       article.appendChild(description);
       description.innerHTML = product.description;
