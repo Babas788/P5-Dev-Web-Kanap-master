e5098b0f6afb4872d03f0df576d83fff8f6f8c12
Babas788 <agi.bastien@hotmail.fr>
2022-11-06 19:01:23 +0100

    commit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,126 @@
-const basket = [];
-cart();
+const myBasket = localStorage.length;
+for (let i = 0; i < myBasket; i++) {
+  const myKey = localStorage.getItem(localStorage.key(i));
+  const product = JSON.parse(myKey);
+  product.forEach((item) => create(item));
+}
 
-basket.forEach((item) => {});
+function create(item) {
+  const article = articleProduct(item);
+  insert(article);
+
+  const image = picture(item);
+  article.appendChild(image);
+
+  const content = itemContent();
+  article.appendChild(content);
+
+  const itemDescription = productDescription();
+  content.appendChild(itemDescription);
+
+  const productName = title(item);
+  itemDescription.appendChild(productName);
+
+  const color = productColor(item);
+  itemDescription.appendChild(color);
+
+  const price = productPrice(item);
+  itemDescription.appendChild(price);
+
+  const settings = itemSetting(item);
+  article.appendChild(settings);
+
+  const deleteItem = deleteProduct();
+  article.appendChild(deleteItem);
+}
+
+function insert(article) {
+  document.getElementById("cart__items").appendChild(article);
+}
+
+function articleProduct(item) {
+  const article = document.createElement("article");
+  article.classList.add("cart__item");
+  article.dataset.id = item.myId;
+  article.dataset.color = item.colors;
+  return article;
+}
+
+function picture(item) {
+  const pictureDiv = document.createElement("div");
+  pictureDiv.classList.add("cart__item__img");
+
+  const imageProduct = document.createElement("img");
+  pictureDiv.appendChild(imageProduct);
+  imageProduct.src = item.image;
+  imageProduct.alt = item.alt;
+
+  return pictureDiv;
+}
+
+function itemContent() {
+  const content = document.createElement("div");
+  content.classList.add("cart__item__content");
+  return content;
+}
+
+function productDescription() {
+  const description = document.createElement("div");
+  description.classList.add("cart__item__content__description");
+  return description;
+}
+
+function title(item) {
+  const name = document.createElement("h2");
+  name.textContent = item.productName;
+  return name;
+}
+
+function productColor(item) {
+  const color = document.createElement("p");
+  color.textContent = item.colors;
+  return color;
+}
+
+function productPrice(item) {
+  const prodPrice = document.createElement("p");
+  prodPrice.textContent = item.productPrice;
+  return prodPrice;
+}
+
+function itemSetting(item, product) {
+  const div = document.createElement("div");
+  div.classList.add("cart__item__content__settings");
+
+  const detailDiv = document.createElement("div");
+  detailDiv.classList.add("cart__item__content__settings__quantity");
+  div.appendChild(detailDiv);
+
+  const qte = document.createElement("p");
+  qte.textContent = "Qté";
+  detailDiv.appendChild(qte);
+
+  const inputQuantity = document.createElement("input");
+  inputQuantity.classList.add("itemQuantity");
+  detailDiv.appendChild(inputQuantity);
+
+  inputQuantity.ariaValueMax = "100";
+  inputQuantity.ariaValueMin = "1";
+  inputQuantity.type = "Number";
+  inputQuantity.name = "itemQuantity";
+  inputQuantity.value = item.quantity;
+
+  return div;
+}
+
+function deleteProduct() {
+  const settingsDelete = document.createElement("div");
+  settingsDelete.classList.add("cart__item__content__settings__delete");
+
+  const deleteItem = document.createElement("p");
+  deleteItem.classList.add("deleteItem");
+  deleteItem.textContent = "Supprimer";
+  settingsDelete.appendChild(deleteItem);
+
+  return settingsDelete;
+}
--- front/js/product.js
+++ front/js/product.js
@@ -1,47 +1,46 @@
-//creation image
-
 // récupération id
 const page = window.location.search;
 const url = new URLSearchParams(page);
 const id = url.get("id");
 const myUrl = `http://localhost:3000/api/products/${id}`;
 
-// récupération de mes data
-let myPrice;
-let picture;
-let accessibility;
-let myName;
-
 fetch(myUrl)
   .then((response) => response.json())
   .then((data) => create(data));
 
 function create(product) {
-  //récup data
+  picture(product);
+  title(product);
+  price(product);
+  description(product);
+  myColor(product);
+  cart(product);
+}
+
+function picture(product) {
+  const image = document.createElement("img");
+  document.querySelector(".item__img").appendChild(image);
+  image.src = product.imageUrl;
+  image.alt = product.altTxt;
+}
+
+function title(product) {
+  const title = document.getElementById("title");
+  title.innerHTML = product.name;
+}
+
+function price(product) {
+  const price = document.getElementById("price");
+  price.innerHTML = product.price;
+}
+
+function description(product) {
+  const description = document.getElementById("description");
+  description.innerHTML = product.description;
+}
+
+function myColor(product) {
   const color = product.colors;
-  myName = product.name;
-  myPrice = product.price;
-  picture = product.imageUrl;
-  accessibility = product.altTxt;
-
-  myPicture(product);
-  insert(product);
-
-  //Mes fonctions
-
-  function myPicture(product) {
-    const image = document.createElement("img");
-    document.querySelector(".item__img").appendChild(image);
-    image.src = product.imageUrl;
-    image.alt = product.altTxt;
-  }
-
-  function insert(product) {
-    document.getElementById("title").innerHTML = product.name;
-    document.getElementById("price").innerHTML = product.price;
-    document.getElementById("description").innerHTML = product.description;
-  }
-
   color.forEach((color) => {
     const option = document.createElement("option");
     document.getElementById("colors").appendChild(option);
@@ -50,40 +49,32 @@ function create(product) {
   });
 }
 
-// récupération données bouton panier
+function cart(product) {
+  const myBasket = document.getElementById("addToCart");
+  myBasket.addEventListener("click", () => {
+    const quantity = document.getElementById("quantity").value;
+    const colors = document.getElementById("colors").value;
 
-const addBasket = document.getElementById("addToCart");
+    let basket = {
+      myId: id,
+      quantity,
+      colors,
+      productPrice: product.price,
+      image: product.imageUrl,
+      productDescription: product.description,
+      productName: product.name,
+      alt: product.altTxt,
+    };
 
-// creation d'un message d'alerte
-
-addBasket.addEventListener("click", (e) => {
-  const productColor = document.getElementById("colors").value;
-  const productQuantity = document.getElementById("quantity").value;
-
-  dataBaket(productColor, productQuantity);
-  if (
-    productColor === null ||
-    productColor === "" ||
-    productQuantity === null ||
-    productQuantity === 0
-  ) {
-    alert("veuillez selectionner une couleur et une quantité valide!");
-    return;
-  }
-});
-
-//récupération des données
-function dataBaket(productColor, productQuantity) {
-  let myData = {
-    id: id,
-    color: productColor,
-    price: myPrice,
-    quantity: productQuantity,
-    picture,
-    accessibility,
-    productTitle: myName,
-  };
-
-  localStorage.setItem(productColor, JSON.stringify(myData));
-  window.location.href = "cart.html";
+    let data = JSON.parse(localStorage.getItem("basket"));
+    if (data) {
+      data.push(basket);
+      localStorage.setItem("basket", JSON.stringify(data));
+    } else {
+      data = [];
+      data.push(basket);
+      localStorage.setItem("basket", JSON.stringify(data));
+    }
+    window.location.href = "cart.html";
+  });
 }
